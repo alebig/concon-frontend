1cf91fdf6ead8a6c970df75fb273877f4d9e9e1a
alebig <alebig.pciencia@gmail.com>
2023-01-18 17:11:20 -0300

    en lectura.js agregado mode:cors y reemplazado localhost por 127.0.0.1

--- src/api/lectura.js
+++ src/api/lectura.js
@@ -1,6 +1,7 @@
 const lectura = async () => {
-    const res = await fetch('http://localhost:4038/api/cafe',
-    {method: 'GET'});
+    const res = await fetch('http://127.0.0.1:4038/api/cafe',
+    {method: 'GET',
+     mode: 'cors'});
     if (res.ok) {
         const registros = await res.json()
         return registros
